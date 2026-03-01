18b95a07f470ca1778bef4dd631eeb1feb796f16
Kaua <kauaalvesgomes2@gmail.com>
2026-03-01 16:37:23 -0300

    New plugins added for nvim: blink (autocompletion), conform (formatter) and treesitter

--- nvim/.config/nvim/lua/core/config.lua
+++ nvim/.config/nvim/lua/core/config.lua
@@ -1,25 +1,27 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.g.have_nerd_foptnt = true
 vim.opt.number = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 -- vim.shoptwmode = false
 vim.opt.relativenumber = true
-vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+vim.schedule(function()
+	vim.o.clipboard = "unnamedplus"
+end)
 vim.opt.breakindent = true
 vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.timeoutlen = 300
 vim.opt.updatetime = 1000
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.list = true
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.confirm = true
 vim.opt.autoindent = true
 vim.opt.tabstop = 4
@@ -30,10 +32,16 @@ vim.opt.smartindent = true
 vim.opt.undolevels = 10000
 vim.opt.termguicolors = true
 vim.opt.wrap = true
-vim.opt.foldmethod = 'indent'
+vim.opt.foldmethod = "indent"
 vim.opt.foldlevel = 99
 vim.opt.smarttab = true
 -- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Moptve focus to the left window' })
 -- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Moptve focus to the right window' })
 -- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Moptve focus to the lower window' })
 -- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Moptve focus to the upper window' })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highligh when yanking text",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
