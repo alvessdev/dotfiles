809a2ad8b077fba1d101a5497c897c625b1ef0c0
Kaua <kauaalvesgomes2@gmail.com>
2026-03-31 09:41:56 -0300

    Updated script to install niri

--- scripts/install-niri.sh
+++ scripts/install-niri.sh
@@ -1,1 +1,4 @@
-sudo pacman -S niri hyprpicker xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk gnome-keyring kitty polkit-kde-agent --needed
+sudo pacman -S niri hyprpicker xwayland-satellite xdg-desktop-portal-gnome \
+	xdg-desktop-portal-gtk gnome-keyring kitty polkit-kde-agent \
+	swaync waypaper swaybg xdg-desktop-portal wl-clipboard cliphist \
+	flameshot playerctl --needed
