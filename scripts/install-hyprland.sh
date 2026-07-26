94b915a32c1d290d20839b799502b0d0395894f0
Kaua <kauaalvesgomes2@gmail.com>
2026-07-26 12:30:17 -0300

    Added swayosd to install-hyprland and niri

--- scripts/install-hyprland.sh
+++ scripts/install-hyprland.sh
@@ -3,6 +3,7 @@ sudo pacman -S --needed --noconfirm stow hyprland swaync awww curl stow \
 	hypridle hyprpolkitagent qt5-wayland qt6-wayland xdg-desktop-portal-hyprland \
 	xdg-desktop-portal-gtk xdg-desktop-portal network-manager-applet wl-clipboard \
 	xdg-utils go cliphist nautilus btop hyprpicker flameshot pavucontrol playerctl \
+	swayosd
 	
 echo dwindle > hypr/.config/hypr/modules/.active_layout
 
--- scripts/install-niri.sh
+++ scripts/install-niri.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo pacman -S niri stow nautilus curl hyprpicker xwayland-satellite xdg-desktop-portal-gnome \
 	xdg-desktop-portal-gtk gnome-keyring kitty polkit-kde-agent \
-	swaync awww xdg-desktop-portal wl-clipboard cliphist \
+	swaync swayosd awww xdg-desktop-portal wl-clipboard cliphist \
 	flameshot playerctl network-manager-applet --needed --noconfirm
 
 sudo ln -s ~/dotfiles/niri/.config/niri/scripts/niri-waybar /usr/local/bin/ &> /dev/null
