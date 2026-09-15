d02479473b06f0c33c9863cb4853ff1879d6acf0
Kaua <kauaalvesgomes2@gmail.com>
2026-09-15 14:52:00 -0300

    nvim: Fixed <S-n> bind

--- nvim/.config/nvim/lua/core/keys.lua
+++ nvim/.config/nvim/lua/core/keys.lua
@@ -1,7 +1,5 @@
 local key = vim.keymap
 
 key.set("i", "hh", "<Esc>")
-key.set("n", "<C-U>", "<C-U>zz")
-key.set("n", "<C-D>", "<C-D>zz")
 key.set("n", "n", "nzz")
-key.set("n", "<S-h>", "Nzz")
+key.set("n", "<S-n>", "Nzz")
