c03bb49b0883fae7137d97b849b6cec6f1eaf94c
Kaua <kauaalvesgomes2@gmail.com>
2026-09-16 13:41:53 -0300

    added make to nvim installation

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo pacman -S --needed --noconfirm neovim ripgrep tar curl ttf-jetbrains-mono-nerd npm unzip
+sudo pacman -S --needed --noconfirm neovim make ripgrep tar curl ttf-jetbrains-mono-nerd npm unzip
 rm -rf ~/.config/nvim &> /dev/null
 stow -d ~/dotfiles/ nvim
